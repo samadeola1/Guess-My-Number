69dc4aec1b5e4d09ad184bd1838c7853e2e37168
samadeola1 <oroguns28@gmail.com>
2025-02-17 18:59:02 +0100

    features:added reset button

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ let checkBtn = document.querySelector(".btn-checker");
 let message = document.querySelector(".message");
 let number = document.querySelector(".number");
 let score = document.querySelector(".score");
+let resetbtn = document.querySelector(".reset-btn")
+
 function gamer(e) {
   e.preventDefault();
   let randomNumGen = Math.ceil(Math.random() * 6);
@@ -13,15 +15,15 @@ function gamer(e) {
   if (!inpOfGuess.value) {
     message.textContent = "input field must not be empty";
   } else if (inpOfGuess.value > randomNumGen) {
-    message.textContent = "you guessed higher";
+    message.textContent = "you guessed higher 🙂";
     number.textContent = randomNumGen;
     score.textContent--;
     body.backgroundColor = "blue";
   } else if (inpOfGuess.value < randomNumGen) {
-    message.textContent = "you guessed lower";
+    message.textContent = "you guessed lower 🙂";
     number.textContent = randomNumGen;
     score.textContent--;
-    body.backgroundColor = "Yellow";
+    body.backgroundColor = "purple";
   } else if (Number(inpOfGuess.value) === randomNumGen) {
      message.textContent = "you guessed right 👍 ";
      number.textContent = randomNumGen;
@@ -30,11 +32,24 @@ function gamer(e) {
 
   if(score.textContent < 1){
     checkBtn.setAttribute("disabled", "disabled");
-    message.textContent = "Game over, restart 👎 ";
-    checkBtn.style.backgroundColor = "grey";
+    message.textContent = "Game over, restart 🔄 ";
+    checkBtn.style.backgroundColor = "grey-600";
     checkBtn.style.cursor = "no-drop";
+    body.backgroundColor = "grey"
+    number.textContent = "?";
+    inpOfGuess.value = ""
 
   }
 }
 
 checkBtn.addEventListener("click", gamer);
+
+
+function resetgame (){
+
+location.reload();
+
+}
+ resetbtn.addEventListener("click", resetgame)
+
+
